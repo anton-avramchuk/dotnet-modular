e1c9760b7c6374fdd370205c3d746418c0f6701d
aavramchuk <aav1984@gmail.com>
2025-01-07 19:22:46 +0200

    add modules

---
+++ src/modules/Dotnet.Modular.Modules.Blazor.Layout.Bootstrap/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Dotnet.Modular.Modules.Blazor.Layout.Bootstrap;
+
+public class Class1
+{
+
+}
---
+++ src/modules/Dotnet.Modular.Modules.Blazor.Layout.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Dotnet.Modular.Modules.Blazor.Layout.Core;
+
+public class Class1
+{
+
+}
