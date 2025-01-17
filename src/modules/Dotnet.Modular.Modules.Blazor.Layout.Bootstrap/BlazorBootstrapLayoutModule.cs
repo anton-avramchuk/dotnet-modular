33e61deffeaae8cc6fc237d1e5efec305dedf7e9
aavramchuk <aav1984@gmail.com>
2025-01-17 18:31:01 +0200

    remove layoput core module

--- src/modules/Dotnet.Modular.Modules.Blazor.Layout.Bootstrap/BlazorBootstrapLayoutModule.cs
+++ src/modules/Dotnet.Modular.Modules.Blazor.Layout.Bootstrap/BlazorBootstrapLayoutModule.cs
@@ -1,9 +1,7 @@
 ﻿using Dotnet.Modular.Core;
-using Dotnet.Modular.Modules.Blazor.Layout.Core;
 
 namespace Dotnet.Modular.Modules.Blazor.Layout.Bootstrap;
 
-[DependsOn(typeof(BlazorLayoutCoreModule))]
 public partial class BlazorBootstrapLayoutModule: ModuleBase
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
