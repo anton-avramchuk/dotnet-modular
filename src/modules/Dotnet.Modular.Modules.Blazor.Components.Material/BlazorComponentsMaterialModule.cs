45daa83442a20842deaa37ef3d01014feff5b89c
aavramchuk <aav1984@gmail.com>
2025-01-17 18:38:30 +0200

    add references

---
+++ src/modules/Dotnet.Modular.Modules.Blazor.Components.Material/BlazorComponentsMaterialModule.cs
@@ -0,0 +1,10 @@
+﻿using Dotnet.Modular.Core;
+using Dotnet.Modular.Modules.Blazor.Components.Core;
+
+namespace Dotnet.Modular.Modules.Blazor.Components.Material;
+
+[DependsOn(typeof(BlazorComponentsCoreModule))]
+public partial class BlazorComponentsMaterialModule : ModuleBase
+{
+
+}
---
+++ src/modules/Dotnet.Modular.Modules.Blazor.Layout.Material/BlazorLayoutMaterialModule.cs
@@ -0,0 +1,8 @@
+﻿using Dotnet.Modular.Core;
+
+namespace Dotnet.Modular.Modules.Blazor.Layout.Material;
+
+public partial class BlazorLayoutMaterialModule : ModuleBase
+{
+
+}
