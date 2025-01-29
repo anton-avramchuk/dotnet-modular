1132fc2200af1b7e8efa9174f352dc5327f97080
aavramchuk <aav1984@gmail.com>
2025-01-29 06:28:45 +0200

    add domain module

---
+++ src/modules/Dotnet.Modular.Modules.Domain/Exceptions/ArgumentLengthException.cs
@@ -0,0 +1,18 @@
+﻿namespace Dotnet.Modular.Modules.Domain.Exceptions
+{
+    public class ArgumentLengthException : Exception
+    {
+
+        public ArgumentLengthException(string name, int length) : base($"Invalid Length of {name}. MaxLength is {length}")
+        {
+            Name = name;
+            Length = length;
+        }
+
+        public string Name { get; private set; }
+
+        public int Length { get; private set; }
+
+
+    }
+}
---
+++ src/modules/Dotnet.Modular.Modules.Domain/Exceptions/ArgumentMinLengthException.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet.Modular.Modules.Domain.Exceptions
+{
+    public class ArgumentMinLengthException : Exception
+    {
+
+        public ArgumentMinLengthException(string name, int length) : base($"Invalid Length of {name}. Min is {length}")
+        {
+        }
+    }
+}
