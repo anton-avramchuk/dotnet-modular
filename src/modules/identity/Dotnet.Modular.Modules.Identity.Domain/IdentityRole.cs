99174b4abcfdbb55e3fa9d4cc147d1962497257f
aavramchuk <aav1984@gmail.com>
2025-01-31 06:53:29 +0200

    add entities

---
+++ src/modules/identity/Dotnet.Modular.Modules.Identity.Domain/IdentityRole.cs
@@ -0,0 +1,72 @@
+﻿using Dotnet.Modular.Modules.Domain;
+using System.Security.Claims;
+
+namespace Dotnet.Modular.Modules.Identity.Domain
+{
+    public class IdentityRole : Entity<Guid>, IAggregateRoot
+    {
+        public virtual string Name { get; protected internal set; }
+
+        public virtual string NormalizedName { get; protected internal set; }
+
+
+        private readonly List<IdentityRoleClaim> _claims = new List<IdentityRoleClaim>();
+
+        public virtual IReadOnlyCollection<IdentityRoleClaim> Claims => _claims;
+
+        protected IdentityRole()
+        {
+
+        }
+
+        protected IdentityRole(Guid id) : base(id)
+        {
+
+        }
+
+
+        public void ChangeName(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            ChangeNormalizedName(name);
+        }
+
+        public void ChangeNormalizedName(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            NormalizedName = value.ToUpperInvariant();
+        }
+
+
+        public IdentityRole(string name) : base(Guid.NewGuid())
+        {
+            ChangeName(name);
+        }
+
+
+        public IdentityRole(Guid id, string name) : base(id)
+        {
+            ChangeName(name);
+        }
+
+
+
+
+        public virtual void AddClaim(Claim claim)
+        {
+            _claims.Add(new IdentityRoleClaim(Id, claim));
+        }
+
+        public virtual void RemoveClaim(Claim claim)
+        {
+            _claims.RemoveAll(c => c.ClaimType == claim.Type && c.ClaimValue == claim.Value);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Name = {Name}";
+        }
+    }
+}
---
+++ src/modules/identity/Dotnet.Modular.Modules.Identity.Domain/IdentityUserRole.cs
@@ -0,0 +1,38 @@
+﻿using Dotnet.Modular.Modules.Domain;
+
+namespace Dotnet.Modular.Modules.Identity.Domain
+{
+    public class IdentityUserRole<TIdentityRole> : Entity where TIdentityRole : IdentityRole
+    {
+        /// <summary>
+        /// Gets or sets the primary key of the user that is linked to a role.
+        /// </summary>
+        public Guid UserId { get; private set; }
+
+
+        /// <summary>
+        /// Gets or sets the primary key of the role that is linked to the user.
+        /// </summary>
+        public Guid RoleId { get; private set; }
+
+        public TIdentityRole Role { get; private set; }
+
+        private IdentityUserRole()
+        {
+
+        }
+
+        public void SetRole(TIdentityRole role)
+        {
+            Role = role;
+            RoleId = role.Id;
+        }
+
+        public IdentityUserRole(Guid userId, TIdentityRole role)
+        {
+            UserId = userId;
+            SetRole(role);
+        }
+
+    }
+}
